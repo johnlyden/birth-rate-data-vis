ae13e0f560e17da30407a55f3874f6a0053131c0
johnlyden <johnlydennc@gmail.com>
2018-03-12 16:00:24 -0500

    added scatterplot component

---
+++ src/scatterplot.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {
+    Hint,
+    HorizontalGridLines,
+    MarkSeries,
+    VerticalGridLines,
+    XAxis,
+    XYPlot,
+    YAxis
+} from 'react-vis';
+import HintContent from './hint-content.js';
